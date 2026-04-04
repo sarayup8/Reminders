ee6f93b591bbb859859eefe38dca232b3dc78743
Sarayu Pesala <sarayup@unc.edu>
2026-04-04 18:29:40 -0400

    Initial Commit

---
+++ Reminders/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Reminders
+//
+//  Created by Sarayu Pesala on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
